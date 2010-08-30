95784f41db21560d6f3b93699748f9dbfb561534
matt@mattbenton.net <matt@mattbenton.net@950d9746-3f6f-11de-bd56-27b9897bf3d5>
2010-08-30 04:02:03 +0000

    Remove tpl directory

--- trunk/www/tpl/mtt/cms__modules__help__blocks__manager_home.mtt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this->buf .= '<h3>Manager Help: Introduction</h3>
-
-<p>
-	Hi there
-</p>';
-
-?>
--- trunk/www/tpl/mtt/examples__Dates.mtt.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$this->buf .= '<h3>Dates</h3>
-
-';
-$this->buf .= $ctx->form->getPreview();
-?>
