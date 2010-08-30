d18c2690cb84eeadb0ed9f77a56644f04d4d4364
matt@mattbenton.net <matt@mattbenton.net@950d9746-3f6f-11de-bd56-27b9897bf3d5>
2010-08-30 04:02:03 +0000

    Remove tpl directory

--- www/tpl/mtt/cms__modules__help__blocks__manager_about_users.mtt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this->buf .= '<h3>About: Users</h3>
-
-<p>
-	Hi there
-</p>';
-
-?>
--- www/tpl/mtt/cms__modules__media__Index.mtt.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$this->buf .= '<h3>Media and Galleries</h3>
-<p>You can upload galleries of images and such here.</p>';
-
-?>
